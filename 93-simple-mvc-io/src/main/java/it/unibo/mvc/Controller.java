4b80afc682ce588eabffd43c300a91f2e40e20f1
Francesco Pazzaglia - francesco.pazzaglia3@studio.unibo.it <francesco.pazzaglia3@studio.unibo.it>
2023-11-20 14:42:07 +0100

    Exercise nr.93: done the first part.

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
 
+public class Controller {
+    private final String FILENAME = "output.txt";
+    
+    private final File current;
+    
+    public Controller(File current) {
+        this.current = current;
+    }
+
+    public File getCurrent() {
+        return this.current;
+    }
+
+    public String getPath() {
+        final String PATH = System.getProperty("user.home")
+            + File.separator
+            + FILENAME;
+        return PATH;
+    }
+
+    public void writeStringOnFile(String stringToWrite){
+        try (PrintStream ps = new PrintStream(this.getPath(), StandardCharsets.UTF_8)) {
+            ps.print(stringToWrite);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
 }
